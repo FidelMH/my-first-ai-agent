7098e1151c9bb589d50550ebd91a2b742c1ae03d
FidelMH <moussahaziri.fidel@gmail.com>
2025-06-01 17:42:40 +0400

    refactor: move crew.ai configuration to ai_agents.py

---
+++ ai_agents.py
@@ -0,0 +1,26 @@
+from config import  OLLAMA_MODEL, LLM_API
+from crewai import Crew, Agent, Task, LLM
+# ----- CONFIGURATION DE CREW.AI -----
+# Assurez-vous d'avoir installé la bibliothèque crewai avec `pip install crewai`
+llm = LLM(
+    model=OLLAMA_MODEL,
+    base_url=LLM_API,  # Assurez-vous que l'API Ollama est en cours d'exécution
+)
+
+mon_agent = Agent(
+    role="Assistant IA",
+    goal="Répondre aux questions factuelles avec des explications claires et concises.",
+    backstory="Je suis un assistant qui connaît tout sur l'histoire, la science et la culture générale.",
+    llm=llm,
+)
+
+question_task = Task(
+    description="{question}",
+    agent=mon_agent,
+    expected_output="la réponse à la question"
+)
+
+crew = Crew(
+    agents=[mon_agent],
+    tasks=[question_task],
+)
--- main.py
+++ main.py
@@ -1,33 +1,6 @@
-from crewai import Crew, Agent, Task, LLM
 import discord
-from config import DISCORD_TOKEN, OLLAMA_MODEL, LLM_API
-
-
-
-# ----- CONFIGURATION DE CREW.AI -----
-# Assurez-vous d'avoir installé la bibliothèque crewai avec `pip install crewai`
-llm = LLM(
-    model=OLLAMA_MODEL,
-    base_url=LLM_API,  # Assurez-vous que l'API Ollama est en cours d'exécution
-)
-
-mon_agent = Agent(
-    role="Assistant IA",
-    goal="Répondre aux questions factuelles avec des explications claires et concises.",
-    backstory="Je suis un assistant qui connaît tout sur l'histoire, la science et la culture générale.",
-    llm=llm,
-)
-
-question_task = Task(
-    description="{question}",
-    agent=mon_agent,
-    expected_output="la réponse à la question"
-)
-
-crew = Crew(
-    agents=[mon_agent],
-    tasks=[question_task],
-)
+from config import DISCORD_TOKEN
+from ai_agents import crew
 
 # ----- FIN DE LA CONFIGURATION -----
 class MyClient(discord.Client):
